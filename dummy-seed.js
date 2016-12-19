ea536e6031d6d6ee42a0b11d9e215f966a8031f4
Bryan Gardner <gardner.bryan22@gmail.com>
2016-12-19 15:07:42 -0500

    seed file with dummy test data

---
+++ dummy-seed.js
@@ -0,0 +1,23 @@
+const dbConnection = require('./config').mongoConnection
+const people = require("./data").people;
+
+dbConnection()
+    .then((db) => {
+        return db.dropDatabase().then(() => {
+            return dbConnection;
+        })
+        .then((db) => {
+            console.log("adding user");
+            return people.addPerson("Test1", "Testing1", "test1@none");
+        })
+        .then(() => {
+            console.log("adding user");
+            return people.addPerson("Test2", "Testing2", "test2@none");
+        })
+        .then(() => {
+            console.log("done seeding");
+            db.close();
+        })
+    }, (error) => {
+        console.error(error);
+    })
